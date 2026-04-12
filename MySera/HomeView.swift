6e432fd613a46102ea59eb19f52e74767c4e815c
Sena <143117667+kafeinsnea@users.noreply.github.com>
2026-04-12 13:51:46 +0300

    Initial commit: MySera iOS app

---
+++ MySera/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  MySera
+//
+//  Created by Sena Çırak on 7.04.2026.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
